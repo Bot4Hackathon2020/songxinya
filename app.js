d1dcf91e4f7aa559f07785e065f58c7f4efa492a
payiz-asj <1778029840@qq.com>
2020-11-29 11:49:01 +0800

    最终提交
    最后一次提交

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-
-App()
+//app.js
+App({
+  onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        env: 'messenger-back-end-8d4m67d9ecedf',
+        traceUser: true,
+      })
+    }
+    this.globalData = {}
+  }
+})
